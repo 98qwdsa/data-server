fa36674c085b9ea2cad01278e28de3d57b621ebf
chunsen.zhao <chunsen.zhao@accenture.com>
2020-10-12 16:28:29 +0800

    add getMyScore service

--- src/data-server.js
+++ src/data-server.js
@@ -10,8 +10,17 @@ const server = {
     this.aerageChangeCb && this.aerageChangeCb(this._Average);
   },
   _addSubject(subject) {
-    this._TotalSubject.push(subject);
-    this.subjectChangeCb && this.subjectChangeCb(this._TotalSubject);
+    try {
+      this._TotalSubject.forEach(e => {
+        if (e.code === subject.code) {
+          throw new Error('你已经添加了这门课程');
+        }
+      })
+      this._TotalSubject.push(subject);
+      this.subjectChangeCb && this.subjectChangeCb(this._TotalSubject);
+    } catch (e) {
+      console.error(e)
+    }
   },
   _removeSubject(subject) {
     this._TotalSubject = this._TotalSubject.filter(e => {
@@ -31,7 +40,16 @@ const server = {
     this.subjectChangeCb && this.subjectChangeCb(this._TotalSubject);
   },
   aerageChangeCb: null,
-  subjectChangeCb: null
+  subjectChangeCb: null,
+  async getMyScore(userInfo, subject) {
+    const { name, email } = userInfo;
+    const { label, score } = subject;
+    const addedScore = parseInt((Math.ceil((Math.random() + 0.5) * 10)) + score, 10);
+
+    return await new Promise((reslove, reject) => {
+      setTimeout(() => { reslove({ msg: `你好 ${name} 同学， 你的科目(${label})的最终成绩为:${addedScore}, 成绩单我们已发送到:${email} 请注意查收!!`, scroe: addedScore }) }, 1000)
+    })
+  }
 };
 
 module.exports = server;
